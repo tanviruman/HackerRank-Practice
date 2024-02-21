985ab5acaa39169f3439d6eb9b982eb92c5a0520
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-02-21 11:37:06 +0600

    Update "Hello World" in C.c

--- C programing/"Hello World" in C.c
+++ C programing/"Hello World" in C.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{   char st[100];
+    scanf("%[^\n]%*c", &st);
+    printf("Hello, World!\n");
+    printf(st);  
+    return 0;
+}
+
 
